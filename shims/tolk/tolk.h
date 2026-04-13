4754f3ef4604932e6f5464cd327d6c9213f8e48d
Ethin Probst <ethindp@pm.me>
2026-04-12 20:20:04 -0500

    Add tolk shim

--- shims/tolk/tolk.h
+++ shims/tolk/tolk.h
@@ -1,44 +1,50 @@
-#pragma once
-#ifndef _TOLK_H_
-#define _TOLK_H_
+// SPDX-License-Identifier: LGPLv3
 
-#ifdef _EXPORTING
-#define TOLK_DLL_DECLSPEC __declspec(dllexport)
+#ifndef TOLK_H
+#define TOLK_H
+
+#if defined(_WIN32)
+#ifdef TOLK_BUILDING
+#define TOLK_API __declspec(dllexport)
 #else
-#define TOLK_DLL_DECLSPEC __declspec(dllimport)
-#endif // _EXPORTING
+#define TOLK_API __declspec(dllimport)
+#endif
 #define TOLK_CALL __cdecl
+#elif defined(__GNUC__) || defined(__clang__)
+#define TOLK_API __attribute__((visibility("default")))
+#define TOLK_CALL
+#else
+#define TOLK_API
+#define TOLK_CALL
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #else
 #include <stdbool.h>
 #include <wchar.h>
-#endif // __cplusplus
+#endif
 
-TOLK_DLL_DECLSPEC void TOLK_CALL Tolk_Load();
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_IsLoaded();
-TOLK_DLL_DECLSPEC void TOLK_CALL Tolk_Unload();
-TOLK_DLL_DECLSPEC void TOLK_CALL Tolk_TrySAPI(bool trySAPI);
-TOLK_DLL_DECLSPEC void TOLK_CALL Tolk_PreferSAPI(bool preferSAPI);
-TOLK_DLL_DECLSPEC const wchar_t * TOLK_CALL Tolk_DetectScreenReader();
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_HasSpeech();
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_HasBraille();
+TOLK_API void TOLK_CALL Tolk_Load(void);
+TOLK_API bool TOLK_CALL Tolk_IsLoaded(void);
+TOLK_API void TOLK_CALL Tolk_Unload(void);
+TOLK_API void TOLK_CALL Tolk_TrySAPI(bool trySAPI);
+TOLK_API void TOLK_CALL Tolk_PreferSAPI(bool preferSAPI);
+TOLK_API const wchar_t *TOLK_CALL Tolk_DetectScreenReader(void);
+TOLK_API bool TOLK_CALL Tolk_HasSpeech(void);
+TOLK_API bool TOLK_CALL Tolk_HasBraille(void);
 #ifdef __cplusplus
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_Output(const wchar_t *str, bool interrupt = false);
+TOLK_API bool TOLK_CALL Tolk_Output(const wchar_t *str, bool interrupt = false);
+TOLK_API bool TOLK_CALL Tolk_Speak(const wchar_t *str, bool interrupt = false);
 #else
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_Output(const wchar_t *str, bool interrupt);
-#endif // __cplusplus
+TOLK_API bool TOLK_CALL Tolk_Output(const wchar_t *str, bool interrupt);
+TOLK_API bool TOLK_CALL Tolk_Speak(const wchar_t *str, bool interrupt);
+#endif
+TOLK_API bool TOLK_CALL Tolk_Braille(const wchar_t *str);
+TOLK_API bool TOLK_CALL Tolk_IsSpeaking(void);
+TOLK_API bool TOLK_CALL Tolk_Silence(void);
 #ifdef __cplusplus
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_Speak(const wchar_t *str, bool interrupt = false);
-#else
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_Speak(const wchar_t *str, bool interrupt);
-#endif // __cplusplus
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_Braille(const wchar_t *str);
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_IsSpeaking();
-TOLK_DLL_DECLSPEC bool TOLK_CALL Tolk_Silence();
-#ifdef __cplusplus
-} // extern "C"
-#endif // __cplusplus
+}
+#endif
 
-#endif // _TOLK_H_
+#endif

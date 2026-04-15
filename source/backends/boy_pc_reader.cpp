b02812bf81b9ea557fa17cf3628f39e6df4d6fab
Ethin Probst <ethindp@pm.me>
2026-04-14 20:02:09 -0500

    Bump version since we fixed BoyPC Reader. Fixes #13

--- source/backends/boy_pc_reader.cpp
+++ source/backends/boy_pc_reader.cpp
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
+// Todo: this file is full of workarounds. Remove them when they are no longer
+// needed.
 
 #include "../simdutf.h"
 #include "backend.h"
@@ -12,9 +14,13 @@
 #include <atomic>
 #include <bitset>
 #include <cassert>
+#include <chrono>
+#include <condition_variable>
 #include <mutex>
+#include <shared_mutex>
 #include <string_view>
 #include <tchar.h>
+#include <thread>
 #include <tlhelp32.h>
 #include <utility>
 #include <windows.h>
@@ -82,18 +88,113 @@ private:
   std::atomic_flag initialized;
   std::atomic_flag speaking;
   BoyCtrlSpeakCompleteFunc complete_callback{nullptr};
+  mutable std::shared_mutex lifecycle_mtx;
+  std::thread watchdog_thread;
+  std::mutex watchdog_wake_mtx;
+  std::condition_variable watchdog_wake;
+  std::atomic_flag watchdog_running;
+
+  bool try_reinit() {
+    BoyCtrlUninitialize();
+    initialized.clear();
+    if (BoyCtrlInitializeU8(nullptr) != e_bcerr_success)
+      return false;
+    if (!BoyCtrlIsReaderRunning()) {
+      BoyCtrlUninitialize();
+      return false;
+    }
+    if (!BoyCtrlSetAnyKeyStopSpeaking(false)) {
+      BoyCtrlUninitialize();
+      return false;
+    }
+    initialized.test_and_set();
+    return true;
+  }
+
+  bool attempt_recovery() {
+    std::unique_lock lock(lifecycle_mtx);
+    if (initialized.test() && BoyCtrlIsReaderRunning())
+      return true;
+    return try_reinit();
+  }
+
+  static bool is_recoverable(BoyCtrlError err) {
+    return err == e_bcerr_fail || err == e_bcerr_unavailable;
+  }
+
+  static BackendError map_error(BoyCtrlError err) {
+    switch (err) {
+    case e_bcerr_fail:
+    case e_bcerr_arg:
+      return BackendError::InternalBackendError;
+    case e_bcerr_unavailable:
+      return BackendError::BackendNotAvailable;
+    default:
+      return BackendError::Unknown;
+    }
+  }
+
+  void watchdog_loop() {
+    constexpr auto healthy_interval = std::chrono::seconds(2);
+    constexpr auto retry_interval = std::chrono::milliseconds(500);
+    constexpr std::uint16_t max_burst = 6;
+    constexpr auto backoff_interval = std::chrono::seconds(5);
+    std::uint16_t consecutive_failures = 0;
+    while (watchdog_running.test(std::memory_order_acquire)) {
+      std::chrono::milliseconds interval = healthy_interval;
+      if (consecutive_failures > 0)
+        interval = consecutive_failures < max_burst ? retry_interval
+                                                    : backoff_interval;
+      {
+        std::unique_lock lock(watchdog_wake_mtx);
+        watchdog_wake.wait_for(lock, interval, [this] {
+          return !watchdog_running.test(std::memory_order_acquire);
+        });
+      }
+      if (!watchdog_running.test(std::memory_order_acquire))
+        break;
+      if (!initialized.test())
+        continue;
+      {
+        std::shared_lock lock(lifecycle_mtx);
+        if (BoyCtrlIsReaderRunning()) {
+          consecutive_failures = 0;
+          continue;
+        }
+      }
+      std::unique_lock lock(lifecycle_mtx);
+      if (initialized.test() && BoyCtrlIsReaderRunning()) {
+        consecutive_failures = 0;
+        continue;
+      }
+      speaking.clear();
+      if (try_reinit())
+        consecutive_failures = 0;
+      else
+        ++consecutive_failures;
+    }
+  }
 
 public:
   void handle_speak_complete([[maybe_unused]] int reason) { speaking.clear(); }
 
   ~BoyPCReaderBackend() override {
+    if (watchdog_running.test()) {
+      watchdog_running.clear();
+      watchdog_wake.notify_all();
+      if (watchdog_thread.joinable())
+        watchdog_thread.join();
+    }
+    {
+      std::unique_lock lock(lifecycle_mtx);
+      if (initialized.test()) {
+        BoyCtrlUninitialize();
+        initialized.clear();
+      }
+    }
     if (complete_callback != nullptr) {
       Slots::release(this);
       complete_callback = nullptr;
-    }
-    if (initialized.test()) {
-      BoyCtrlUninitialize();
-      initialized.clear();
     }
   }
 
@@ -131,26 +232,15 @@ public:
   }
 
   BackendResult<> initialize() override {
-    if (initialized.test()) {
+    if (initialized.test())
       return std::unexpected(BackendError::AlreadyInitialized);
-    }
-    if (complete_callback != nullptr) {
+    if (complete_callback == nullptr) {
       complete_callback = Slots::acquire(this);
-      if (complete_callback != nullptr) {
+      if (complete_callback == nullptr)
         return std::unexpected(BackendError::InternalBackendLimitExceeded);
-      }
     }
-    if (const auto res = BoyCtrlInitializeU8(nullptr); res != e_bcerr_success) {
-      switch (res) {
-      case e_bcerr_fail:
-      case e_bcerr_arg:
-        return std::unexpected(BackendError::InternalBackendError);
-      case e_bcerr_unavailable:
-        return std::unexpected(BackendError::BackendNotAvailable);
-      default:
-        return std::unexpected(BackendError::Unknown);
-      }
-    }
+    if (const auto res = BoyCtrlInitializeU8(nullptr); res != e_bcerr_success)
+      return std::unexpected(map_error(res));
     if (!BoyCtrlIsReaderRunning()) {
       BoyCtrlUninitialize();
       return std::unexpected(BackendError::BackendNotAvailable);
@@ -160,31 +250,14 @@ public:
       return std::unexpected(BackendError::BackendNotAvailable);
     }
     initialized.test_and_set();
+    watchdog_running.test_and_set(std::memory_order_release);
+    watchdog_thread = std::thread(&BoyPCReaderBackend::watchdog_loop, this);
     return {};
   }
 
   BackendResult<> speak(std::string_view text, bool interrupt) override {
-    if (!initialized.test()) {
+    if (!initialized.test())
       return std::unexpected(BackendError::NotInitialized);
-    }
-    if (!BoyCtrlIsReaderRunning()) {
-      return std::unexpected(BackendError::BackendNotAvailable);
-    }
-    // Temporary workaround to ensure interrupt always stops
-    // Todo: remove this when fixed upstream
-    if (interrupt) {
-      if (const auto res = BoyCtrlStopSpeaking(false); res != e_bcerr_success) {
-        switch (res) {
-        case e_bcerr_fail:
-        case e_bcerr_arg:
-          return std::unexpected(BackendError::InternalBackendError);
-        case e_bcerr_unavailable:
-          return std::unexpected(BackendError::BackendNotAvailable);
-        default:
-          return std::unexpected(BackendError::Unknown);
-        }
-      }
-    }
     const auto len = simdutf::utf16_length_from_utf8(text.data(), text.size());
     std::wstring wstr;
     wstr.resize(len);
@@ -193,20 +266,47 @@ public:
             reinterpret_cast<char16_t *>(wstr.data()));
         res == 0)
       return std::unexpected(BackendError::InvalidUtf8);
-    if (const auto res = BoyCtrlSpeak(wstr.c_str(), false, !interrupt, true,
-                                      complete_callback);
-        res != e_bcerr_success) {
-      switch (res) {
-      case e_bcerr_fail:
-      case e_bcerr_arg:
-        return std::unexpected(BackendError::InternalBackendError);
-      case e_bcerr_unavailable:
-        return std::unexpected(BackendError::BackendNotAvailable);
-      default:
-        return std::unexpected(BackendError::Unknown);
+    bool needs_recovery = false;
+    {
+      std::shared_lock lock(lifecycle_mtx);
+      if (!BoyCtrlIsReaderRunning()) {
+        needs_recovery = true;
+      } else {
+        // Temporary workaround to ensure interrupt always stops
+        // Todo: remove this when fixed upstream
+        if (interrupt) {
+          if (const auto res = BoyCtrlStopSpeaking(false);
+              res != e_bcerr_success) {
+            if (!is_recoverable(res))
+              return std::unexpected(map_error(res));
+            needs_recovery = true;
+          }
+        }
+        if (!needs_recovery) {
+          const auto res = BoyCtrlSpeak(wstr.c_str(), false, !interrupt, true,
+                                        complete_callback);
+          if (res == e_bcerr_success) {
+            speaking.test_and_set();
+            return {};
+          }
+          if (!is_recoverable(res))
+            return std::unexpected(map_error(res));
+          needs_recovery = true;
+        }
       }
     }
-    speaking.test_and_set();
+    if (needs_recovery) {
+      if (!attempt_recovery())
+        return std::unexpected(BackendError::BackendNotAvailable);
+      std::shared_lock lock(lifecycle_mtx);
+      if (interrupt)
+        BoyCtrlStopSpeaking(false);
+      if (const auto res = BoyCtrlSpeak(wstr.c_str(), false, !interrupt, true,
+                                        complete_callback);
+          res != e_bcerr_success)
+        return std::unexpected(map_error(res));
+      speaking.test_and_set();
+    }
     return {};
   }
 
@@ -215,33 +315,35 @@ public:
   }
 
   BackendResult<bool> is_speaking() override {
-    if (!initialized.test()) {
+    if (!initialized.test())
       return std::unexpected(BackendError::NotInitialized);
-    }
-    if (!BoyCtrlIsReaderRunning()) {
+    std::shared_lock lock(lifecycle_mtx);
+    if (!BoyCtrlIsReaderRunning())
       return std::unexpected(BackendError::BackendNotAvailable);
-    }
     return speaking.test();
   }
 
   BackendResult<> stop() override {
-    if (!initialized.test()) {
+    if (!initialized.test())
       return std::unexpected(BackendError::NotInitialized);
-    }
-    if (!BoyCtrlIsReaderRunning()) {
-      return std::unexpected(BackendError::BackendNotAvailable);
-    }
-    if (const auto res = BoyCtrlStopSpeaking(false); res != e_bcerr_success) {
-      switch (res) {
-      case e_bcerr_fail:
-      case e_bcerr_arg:
-        return std::unexpected(BackendError::InternalBackendError);
-      case e_bcerr_unavailable:
-        return std::unexpected(BackendError::BackendNotAvailable);
-      default:
-        return std::unexpected(BackendError::Unknown);
+    {
+      std::shared_lock lock(lifecycle_mtx);
+      if (BoyCtrlIsReaderRunning()) {
+        const auto res = BoyCtrlStopSpeaking(false);
+        if (res == e_bcerr_success) {
+          speaking.clear();
+          return {};
+        }
+        if (!is_recoverable(res))
+          return std::unexpected(map_error(res));
       }
     }
+    if (!attempt_recovery())
+      return std::unexpected(BackendError::BackendNotAvailable);
+    std::shared_lock lock(lifecycle_mtx);
+    if (const auto res = BoyCtrlStopSpeaking(false); res != e_bcerr_success)
+      return std::unexpected(map_error(res));
+    speaking.clear();
     return {};
   }
 };

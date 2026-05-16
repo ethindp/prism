c78d0b4bdd9a486e52b23749cffeb55b593e7084
Ethin Probst <ethindp@pm.me>
2026-05-16 13:39:08 -0500

    restructure: move third-party deps to top-level third_party/

--- third_party/simdutf/simdutf.cpp
+++ third_party/simdutf/simdutf.cpp
@@ -1,4 +1,4 @@
-/* auto-generated on 2026-04-25 17:56:22 -0400. Do not edit! */
+/* auto-generated on 2026-05-07 06:54:06 +0200. Do not edit! */
 /* begin file src\simdutf.cpp */
 #include "simdutf.h"
 

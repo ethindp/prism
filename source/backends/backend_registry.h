c8783e5ce0e5392cfd6e63a6379ce080c200bdc7
Ethin Probst <ethindp@pm.me>
2026-05-22 09:12:32 -0500

    Switch to using C++ attribute syntax

--- source/backends/backend_registry.h
+++ source/backends/backend_registry.h
@@ -168,13 +168,10 @@ template <typename T> struct BackendRegistrar {
   }
 };
 
-#if defined(__GNUC__) || defined(__clang__)
-  #define PRISM_KEEP __attribute__((used, retain))
-#else
-  #define PRISM_KEEP
-#endif
 #define REGISTER_BACKEND(cls, name, priority)                                  \
-  PRISM_KEEP static ::BackendRegistrar<cls> registrar_##cls##_(name, priority)
+  [[gnu::used, gnu::retain]] static ::BackendRegistrar<cls>                    \
+  registrar_##cls##_(name, priority)
 
 #define REGISTER_BACKEND_WITH_ID(cls, id, name, priority)                      \
-  PRISM_KEEP static ::BackendRegistrar<cls> registrar_##cls##_(id, name, priority)
+  [[gnu::used, gnu::retain]] static ::BackendRegistrar<cls>                    \
+  registrar_##cls##_(id, name, priority)
